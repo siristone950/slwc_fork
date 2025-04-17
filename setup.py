90ac970be845feab57d16c688cc66dc5e2a97cd7
siristone950 <mestephan20@gmail.com>
2025-04-16 20:12:23 -0500

    changing the names

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="streamlit-lightweight-charts-ntf",
+    name="slwc_fork",
     version="0.8.12",
     author="Joe Rosa",
     author_email="joe.rosa@itpmngt.co.uk",
@@ -17,10 +17,10 @@ setuptools.setup(
     description="Wrapper for TradingView lightweight-charts using ntf fork",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/freyastreamlit/streamlit-lightweight-charts-ntf",
-    packages=['streamlit_lightweight_charts_ntf'],
+    url="https://github.com/siristone950/slwc_fork",
+    packages=['slwc_fork'],
     package_data={
-        'streamlit_lightweight_charts_ntf': ['frontend/build/*','frontend/build/static/js/*'],
+        'slwc_fork': ['frontend/build/*','frontend/build/static/js/*'],
     },
     include_package_data=True,
     python_requires=">=3.6",
